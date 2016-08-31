108010f77e748b74d643fe967dc8e2f1a049ca7f
killa2471 <xbox360plzzz@hotmail.com>
2016-08-31 17:42:06 +1200

    Added the awesome baloon (arrow keys use them)

---
+++ Proto_4/Assets/Juanes_files/Move_sphere.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move_sphere : MonoBehaviour {
+	Rigidbody rigid;
+	public float Speed;
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+
+		float moveHorizontal = Input.GetAxis("Horizontal") * Speed;
+		float moveVertical = Input.GetAxis("Vertical") * Speed;
+
+	Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+
+		rigid.AddForce(movement);
+}
+}
---
+++ Proto_4/Assets/Juanes_files/getrigid.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getrigid : MonoBehaviour {
+	HingeJoint hindge;
+	// Use this for initialization
+	void Start () {
+		hindge = GetComponent<HingeJoint> ();
+		hindge.connectedBody = transform.parent.gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
