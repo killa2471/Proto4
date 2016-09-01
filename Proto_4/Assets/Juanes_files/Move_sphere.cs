f21551ca6036da3d594b7da787a3ac2f5f186f4a
Joshua Tanner <mr-jt-96@hotmail.com>
2016-09-01 21:22:13 +1200

    Vertical movement added

--- Proto_4/Assets/Juanes_files/Move_sphere.cs
+++ Proto_4/Assets/Juanes_files/Move_sphere.cs
@@ -4,20 +4,30 @@ using System.Collections;
 public class Move_sphere : MonoBehaviour {
 	Rigidbody rigid;
 	public float Speed;
+
+ 
+
 	// Use this for initialization
 	void Start () {
+
 		rigid = GetComponent<Rigidbody> ();
+      
+
 	}
 	
+   
+
+
 	// Update is called once per frame
 	void FixedUpdate ()
-	{
+	{      
 
 		float moveHorizontal = Input.GetAxis("Horizontal") * Speed;
 		float moveVertical = Input.GetAxis("Vertical") * Speed;
+      
 
-	Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
+	    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        Debug.Log(movement);
 
 		rigid.AddForce(movement);
 }
---
+++ Proto_4/Assets/MoveBalloon.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBalloon : MonoBehaviour {
+
+    public float currentTemp;
+    public float currentMass;
+    public float currentFuel = 1000;
+    public float currentHeight;
+    public float heightToReach;
+
+	// Use this for initialization
+	void Start () {
+        currentTemp = 0.1f;
+        currentMass = 20.0f;
+	}
+
+    float GetYChange()
+    {
+        //Fuel is being used. 
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentFuel -= 0.1f;
+            if (currentTemp < 100)
+                currentTemp += 0.4f;
+        }
+        else if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            if (currentTemp > -10.0)
+                currentTemp -= 0.4f;
+        }
+        else
+        {
+            currentTemp -= 0.1f;
+        }
+        return currentTemp / (currentMass * 2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveY = GetYChange();
+
+        Vector3 movement = new Vector3(0.0f, moveY, 0.0f);
+        Debug.Log(moveY);
+        if(!(transform.position.y <= 0 && currentTemp <= 0.0f))
+        transform.position = new Vector3(transform.position.x, transform.position.y + moveY, transform.position.z);
+    }
+   
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
