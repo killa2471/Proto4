e897e415f5828f5989f913c7a8662a8eb75c9890
killa2471 <xbox360plzzz@hotmail.com>
2016-09-03 13:03:55 +1200

    Fully rigged and jointed balloon

---
+++ Proto_4/Assets/Juanes_files/Code/getrigid.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getrigid : MonoBehaviour {
+	HingeJoint hindge;
+	// Use this for initialization
+	void Start () {
+		hindge = GetComponent<HingeJoint> ();
+		hindge.connectedBody = transform.parent.gameObject.GetComponent<Rigidbody> ();
+	}
+}
---
+++ Proto_4/Assets/Juanes_files/Code/getrigid_down.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class getrigid_down : MonoBehaviour {
+	HingeJoint hindge;
+	// Use this for initialization
+	void Start () {
+		hindge = GetComponent<HingeJoint> ();
+		hindge.connectedBody = transform.GetChild(0).gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
